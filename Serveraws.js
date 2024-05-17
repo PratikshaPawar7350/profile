23f8932d2be0e6a953db0e9743f4b90189894dc0
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-05-17 12:25:42 +0530

    Update Serveraws.js

--- Serveraws.js
+++ Serveraws.js
@@ -19,7 +19,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: 10,
+  connectionLimit: 100,
   authPlugins: {
     mysql_clear_password: () => () => Buffer.from(process.env.DB_PASSWORD + '\0')
   }
